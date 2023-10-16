ce8274895bd7e85d71728e1d8b39d49022654197
francisz3 <s3971510@student.rmit.edu.au>
2023-10-16 15:22:04 +1100

    Finish all testing methods, fixed date bug

--- src/main/java/LibraryCard.java
+++ src/main/java/LibraryCard.java
@@ -4,11 +4,11 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Library Card associated with the Student 
+ * Library Card associated with the Student
  */
 public class LibraryCard {
     /**
-     * Card id 
+     * Card id
      */
     private int ID;
 
@@ -33,20 +33,16 @@ public class LibraryCard {
     private double fine;
 
     /**
-     *  Details about the cardholder
+     * Details about the cardholder
      */
     private Student student;
-
-
-
 
     public LibraryCard(Student student, Date IssueDate, Date ExpiryDate, int ID) {
         this.student = student;
         this.IssueDate = IssueDate;
-	   this.ExpiryDate = ExpiryDate;
-	   this.ID = ID;
+        this.ExpiryDate = ExpiryDate;
+        this.ID = ID;
     }
-
 
     public int getID() {
         return this.ID;
@@ -64,7 +60,6 @@ public class LibraryCard {
         this.fine = fine;
     }
 
-
     public Date getIssueDate() {
         return IssueDate;
     }
@@ -81,25 +76,76 @@ public class LibraryCard {
         this.ExpiryDate = ExpiryDate;
     }
 
-    
     public List<Book> getBooks() {
         return borrowed;
     }
 
-    
+    // add books to borrowed list
+    public void setBooksBorrowed(ArrayList<Book> booklist) {
+        this.borrowed = booklist;
+    }
+
+    public void addBorrowbook(Book book) {
+        borrowed.add(book);
+    }
 
     /**
      * Issue a new book
-     * @param Book: book to borrow 
+     * 
+     * @param Book: book to borrow
      * @return true if the book is successfully borrowed, false otherwise
      */
 
-    public boolean issueBook(Book book){
-    	return false;
-   
+    public boolean issueBook(Book book) throws IllegalBookIssueException {
+
+        // Get the number of books borrowed by the student on the library card
+        int numBooksBorrowed = borrowed.size();
+
+        // The number of books borrowed should not be greater than 4
+        if (numBooksBorrowed > 4) {
+            return false;
+        }
+
+        // Throws an exception if the same book is already issued on the library card
+
+        if (borrowed.contains(book)) {
+            throw new IllegalBookIssueException("Book is already issued on this library card");
+        }
+
+        // Check that the library card is still valid
+
+        Date currDate = new Date();
+        int isValid = currDate.compareTo(this.ExpiryDate);
+        if (isValid > 0) {
+            // isValid will be greater than 0 if the current date exceeds the expiry date
+            return false;
+        }
+
+        // Check that the book is available for borrowing
+        if (book.getStatus() == false) {
+            return false;
+        }
+
+        // Check if library card has pending fines
+        if (this.fine > 0.0) {
+            return false;
+        }
+
+        // Check demand, set days based off demand
+        if (book.getDemand() == 0) {
+            book.setDays(15);
+        } else if (book.getDemand() == 1) {
+            book.setDays(3);
+        }
+
+        // if method has yet to return false
+
+        // update relevant variables - add to borrowed, Not available
+        borrowed.add(book);
+        book.setStatus(false);
+
+        return true;
+
     }
-
-
-
 
 }
--- src/test/java/IssueBook.java
+++ src/test/java/IssueBook.java
@@ -5,19 +5,159 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
- *  Implement and test {Programme.addStudent } that respects the considtion given the assignment specification
- * NOTE: You are expected to verify that the constraints to borrow a new book from a library
+ * Implement and test {Programme.addStudent } that respects the considtion given
+ * the assignment specification
+ * NOTE: You are expected to verify that the constraints to borrow a new book
+ * from a library
  *
  * Each test criteria must be in an independent test method .
  *
  * Initialize the test object with "setting" method.
  */
 public class IssueBook {
-	
-	
+    private LibraryCard libraryCard;
+    private Student student;
 
+    @BeforeEach
+    public void setUp() {
+        // Instantiate Dates for library card
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(2023, Calendar.JANUARY, 1);
+        Date issueDate = calendar.getTime();
+
+        calendar.set(2024, Calendar.OCTOBER, 1);
+        Date expiryDate = calendar.getTime();
+
+        Student student = new Student("Bob", 123);
+        libraryCard = new LibraryCard(student, issueDate, expiryDate, 123);
+    }
+
+    @Test
+    public void ReturnsFalse_IfBooksGreaterFour() throws IllegalBookIssueException {
+        // Tests whether issuing a book whilst exceeding the max amount of four
+        // borrowed books, will return false
+
+        // Init borrowed bookslist
+        ArrayList<Book> borrowed_books = new ArrayList<Book>();
+
+        borrowed_books.add(new Book(497, "Whispers in Moonlight", 0));
+        borrowed_books.add(new Book(334, "The Enchanted Atlas", 1));
+        borrowed_books.add(new Book(125, "Chronicles of Clockwork Kingdom", 0));
+        borrowed_books.add(new Book(193, "Echos Of Elysium", 0));
+        borrowed_books.add(new Book(183, "Tales of Forgotten Stars", 0));
+
+        libraryCard.setBooksBorrowed(borrowed_books);
+        boolean expected = false;
+
+        Book book6 = new Book(3, "art", 0);
+        boolean actual = libraryCard.issueBook(book6);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ThrowsException_IfIllegalBookException() {
+        // Tests if an exception will be thrown if a book is already
+        // within the the cards borrowed books
+
+        // Init borrowed list of books and add a book to be borrowed
+        ArrayList<Book> borrowed_books = new ArrayList<Book>();
+        Book book1 = new Book(0, "Throw Away", 0);
+        borrowed_books.add(book1);
+        libraryCard.setBooksBorrowed(borrowed_books);
+
+        // Try to issue this same book again
+        assertThrows(IllegalBookIssueException.class, () -> {
+            libraryCard.issueBook(book1);
+        });
+    }
+
+    @Test
+    public void ReturnFalse_IfInvalidCard() throws IllegalBookIssueException {
+        // Tests if an invalid card, i.e. a card with an expiry date exceeding current
+        // date
+        // will return false if invalid card
+
+        // Init date objects to set expiry date
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2023, Calendar.OCTOBER, 1);
+        Date oldDate = calendar.getTime();
+        libraryCard.setExpiryDate(oldDate);
+
+        // expect to return false due to expired date exceeded by current
+        Book book1 = new Book(0, "Dated", 0);
+
+        // Set expected and actual variables
+        boolean expected = false;
+        boolean actual = libraryCard.issueBook(book1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ReturnFalse_IfBookUnavailable() throws IllegalBookIssueException {
+        // Tests if an issue of an unavailble book returns false
+
+        // Init a book a set it to unavailable
+        Book book1 = new Book(0, "Unattainable", 0);
+        book1.setStatus(false);
+
+        // Set expected and actual variables
+        boolean expected = false;
+        boolean actual = libraryCard.issueBook(book1);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ReturnFalse_IfPendingFine() throws IllegalBookIssueException {
+        // Tests if issueing a book with a pending fine returns false
+
+        // Init update fine on library card
+        libraryCard.setFine(50.00);
+        Book book1 = new Book(0, "How to pay Fines", 0);
+
+        // Set expected and actual variables
+        boolean expected = false;
+        boolean actual = libraryCard.issueBook(book1);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void Return3_IfDemandHigh() throws IllegalBookIssueException {
+        // Tests if a books return day is within 3 days due to high demand (1)
+
+        // Init book object with a demand of 1
+        Book book1 = new Book(0, "In Demand", 1);
+        libraryCard.issueBook(book1);
+
+        // Set expected and actual variables
+        int expected = 3;
+        int actual = book1.getDays();
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void ReturnTrue_IfPassAllConstraints() throws IllegalBookIssueException {
+        // Tests if a issued book returns true if all constraints are met
+        Book book1 = new Book(0, "How to pass tests", 0);
+
+        // Set expected and actual variables
+        boolean expected = true;
+        boolean actual = libraryCard.issueBook(book1);
+
+        assertEquals(expected, actual);
+    }
 
 }
